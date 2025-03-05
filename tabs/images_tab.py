cab3166026e49c2e98b53d89d139142e9842afaf
Brice Piaple Dada <epiab002@cegep-rimouski.qc.ca>
2025-03-04 23:34:53 -0500

    Mise à jour de l'outil de génération d'images : ajout de la prise en charge des étapes d'inférence et changement de pipeline pour Stable Diffusion.

--- tabs/images_tab.py
+++ tabs/images_tab.py
@@ -1,11 +1,10 @@
 from email.mime import image
-from exceptiongroup import catch
 import gradio as gr
-from utils.file_utils import load_images
 
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
-image_generator = DiffusionPipeline.from_pretrained("CompVis/ldm-text2im-large-256")
+# image_generator = DiffusionPipeline.from_pretrained("CompVis/ldm-text2im-large-256")
+image_generator = StableDiffusionPipeline.from_single_file("./models/civitai/Stable-diffusion/v1-5-pruned-emaonly.safetensors")
 # image_generator.device = "cpu"
 image_generator.to("cpu")
 
@@ -17,13 +16,11 @@ def create_images_tab():
         gr.Markdown("## Images et médias")
         
         with gr.Tabs():
-            with gr.TabItem("Télécharger"):
-                image_upload = gr.File(label="Télécharger vos propres images", file_types=["image"])
-            
             with gr.TabItem("Générer"):
                 with gr.Row():
                     # Composant d'image à gauche pour afficher l'image générée
                     generated_image = gr.Image(label="Image générée", interactive=False)
+                    inference_steps = gr.Slider(label="Nombre d'étapes d'inférence", minimum=1, maximum=30, value=10)
                     
                     # Contrôles à droite
                     with gr.Column():
@@ -40,9 +37,14 @@ def create_images_tab():
                         generate_image_btn = gr.Button("Générer une image", elem_classes="secondary-button")
                 
                 # Fonction pour générer l'image
-                def generate_image(prompt, negative):
+                def generate_image(prompt, negative, inference_steps=10):
                     try:
-                        image = image_generator(prompt, negative).images[0]
+                        image = image_generator(
+                            prompt=prompt, 
+                            negative_promt=negative,
+                            num_inference_steps=inference_steps,
+
+                            ).images[0]
                         return image
                     except Exception as e:
                         return f"Erreur lors de la génération de l'image: {str(e)}"
@@ -50,7 +52,7 @@ def create_images_tab():
                 # Connection du bouton à la fonction de génération
                 generate_image_btn.click(
                     fn=generate_image, 
-                    inputs=[image_prompt, negative_promt], 
+                    inputs=[image_prompt, negative_promt, inference_steps], 
                     outputs=[generated_image]
                 )
    
